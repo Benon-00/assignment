4c75c2f91dafb43e39c85153b5d0e967d94a0b27
Benon-00 <75932024+Benon-00@users.noreply.github.com>
2026-08-02 09:54:24 +0300

    Add files via upload

---
+++ examples/degrees_fun copy.py
@@ -0,0 +1,9 @@
+## Example
+
+import math
+
+print(math.degrees(0.6754))
+print(math.degrees(1.543))
+print(math.degrees(25))
+
+
---
+++ examples/rad_fun copy.py
@@ -0,0 +1,9 @@
+## Example
+
+import math
+
+print(math.radians(0.6754))
+print(math.radians(1.543))
+print(math.radians(25))
+
+
