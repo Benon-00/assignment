c17b7c74f552c8585059bbf0c757c14e057bc892
Benon-00 <75932024+Benon-00@users.noreply.github.com>
2026-08-08 14:32:50 +0300

    Add files via upload

---
+++ examples/np_random.rand copy.py
@@ -0,0 +1,8 @@
+## Example
+
+import numpy as np
+
+np.random.seed(100)
+arr = np.random.rand(2, 2)
+print(arr)
+
---
+++ examples/np_sort copy.py
@@ -0,0 +1,15 @@
+## Example
+
+import numpy as np
+
+arr = np.array([1,4,7,2,5,10])
+
+sorted = np.sort(
+    a = arr,
+    axis = None,
+    kind = None,
+    order = None
+)
+print(sorted)
+
+
